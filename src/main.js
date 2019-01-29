1886d35408551b25bb46607b835bfeb165e06d04
RaquelCC <rcanalesconcha@gmail.com>
2019-01-29 17:19:53 -0300

    boilerplate ready

---
+++ src/main.js
@@ -0,0 +1,42 @@
+document.addEventListener("DOMContentLoaded", event => {
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyAzb47AU23OXlHs6sjt-uGq9ZcbwuaBVTg",
+    authDomain: "red-social-profesores.firebaseapp.com",
+    databaseURL: "https://red-social-profesores.firebaseio.com",
+    projectId: "red-social-profesores",
+    storageBucket: "",
+    messagingSenderId: "847131414857"
+  };
+  firebase.initializeApp(config);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
