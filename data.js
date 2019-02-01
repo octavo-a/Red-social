63e776a84e247f682d05c3382e581e314daaa77a
RaquelCC <rcanalesconcha@gmail.com>
2019-01-31 23:17:03 -0300

    creada pagina de inicio + side nav responsive (no todo funcional, solo estructura)

--- data.js
+++ data.js
@@ -1,4 +1,37 @@
-window.socialNetrowk = {
+let userNow;
+window.socialNetwork = {
+
+    createNewUserStorage: ()=> {
+        firebase.database().ref("users/"+firebase.auth().currentUser.uid).once("value", function(snapshot) {
+            if (snapshot.val() === null) {
+                firebase.database().ref("/users/" + firebase.auth().currentUser.uid).set({
+                    "profile": {
+                        "username": firebase.auth().currentUser.displayName !== null ? firebase.auth().currentUser.displayName : firebase.auth().currentUser.email,
+                        "especialidad": false,
+                        "profilePic": firebase.auth().currentUser.photoURL,
+                        "email": firebase.auth().currentUser.email
+                    },
+                    "posts": false,
+                    "comments": false
+                })
+            }
+            else {
+                // console.log(snapshot.val())
+                // console.log("usuario ya existia")
+            }
+            return snapshot
+        })
+        .then(user=> {
+            userNow = user.val()
+        })
+
+        return userNow;
+        
+    },
+
+    getPosts: ()=> {
+        
+    }
 
 
 
@@ -7,4 +40,58 @@ window.socialNetrowk = {
 
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// {
+//     /* Visit https://firebase.google.com/docs/database/security to learn more about security rules. */
+//     "rules": {
+//       ".read": false,
+//       ".write": false,
+//         "users": {
+//           "$uid": {
+//             "profile": {
+//                ".validate": "auth.uid == $uid"
+//             },
+//                "posts": {
+//                   "tags": {
+//                    ".validate": "auth.uid == $uid"
+//                 },
+//                 "content": {
+//                   ".validate": "auth.uid == $uid"
+//                 },
+//                 "comments": {
+//                   "$commentid": {
+//                       ".validate": "auth.uid != null && newData.child('user_id').val() == auth.uid"
+                  
+//                   }
+//                 }
+//             }
+//           },
+//         ".read": "auth.uid != null",
+//         ".write": "auth.uid != null",
+            
+//         }
+//     }
+//   }
+
+
+
+
 
--- main.js
+++ main.js
@@ -9,43 +9,102 @@ const config = {
     messagingSenderId: "847131414857"
   };
   firebase.initializeApp(config);
+  loginPage();
 
-
-  // LOGIN CON GOOGLE
-  document.getElementById("google-login").addEventListener("click", ()=> {
-    const provider = new firebase.auth.GoogleAuthProvider();
-    firebase.auth().signInWithRedirect(provider);
-  })
-
-  // LOGIN CON FACEBOOK
-  document.getElementById("facebook-login").addEventListener("click", ()=> {
-    const provider = new firebase.auth.FacebookAuthProvider();
-    firebase.auth().signInWithRedirect(provider);
-  })
-
-  // PARA LOGIN CON EMAIL Y PASSWORD
-  document.getElementById("login-btn").addEventListener("click", ()=> {
-    const email = document.getElementById("login-mail").value;
-    const password = document.getElementById("login-pwd").value;
-    firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
-      // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      console.log(`${errorCode} ${errorMessage}`)
-      // ...
-    });
-  })
+  // FUNCION QUE CREA PAGINA INICIAL
+  function loginPage() {
+    document.getElementById("root").innerHTML = `
+    <div id="logins">
+    <input type="text"  id="login-mail" class="login-input" placeholder="Correo electrónico..">
+    <input type="password" id="login-pwd" class="login-input" placeholder="Contraseña..">
+    <button id="login-btn" class="login-btn">Iniciar Sesión</button>
+    <span id="google-login" class="login"><img src="img/googleLogo.png" class="icon" alt="googleLogo"><p>Iniciar Sesión con Google</p></span>
+    <span id="facebook-login" class="login facebook"><img src="img/facebookLogo2.png" class="icon" alt="facebookLogo"><p>Iniciar Sesión con Facebook</p></span>
+    </div>
+    
+    `
+    // BOTON LOGIN CON GOOGLE
+    document.getElementById("google-login").addEventListener("click", googleLogin)
   
-  // PARA VER QUE SE LOGEO CORRECTAMENTE
+    // BOTON LOGIN CON FACEBOOK
+    document.getElementById("facebook-login").addEventListener("click", facebookLogin)
+  
+    // BOTON PARA LOGIN CON EMAIL Y PASSWORD
+    document.getElementById("login-btn").addEventListener("click", ()=> {
+      const email = document.getElementById("login-mail").value;
+      const password = document.getElementById("login-pwd").value;
+      emailLogin(email, password);
+    })
+
+  }
+
+
+  
+
+  // LO QUE SE CREA CUANDO LOGEA EL USUARIO
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      // User is signed in.
+      window.socialNetwork.createNewUserStorage(user);
+      document.getElementById("root").innerHTML = `
+      
+      <nav class="responsive">
+      <div id="div-logo">
+          <img id="logo" src="./img/teachersLogo.png" alt="logo">
+      </div>
+      <div id="search-nav"><input id="search" type="text" placeholder="Buscar.."><a><i class="fas fa-search fa-lg"></i></a></div>
+     </nav>
+
+     <div id="content">
+         <div class="post">
+             <div class="post-header">
+                 <span><img src="./img/userLogo.png" class="user-pic-post" alt="userPic"><p>Raquel Patricia Canales Concha - Profesora de Básica</p></span>
+
+             </div>
+             <div class="post-content">
+              <span>Me gustaría que pudieran sugerirme actividades para realizar en el aula con niños de 4to Básico, estamos viendo los recursos naturales de las regiones a lo largo del país, gracias!</span>
+             </div>
+         </div>
+
+     </div>
+     <div id="user-profile-side-nav">
+         <div id="user-pic"><img src="./img/userLogo.png" class="user-pic" alt="userPic"></div>
+         <div id="user-name">Raquel Patricia Canales Concha</div>
+         <div class="side-option"><a>Perfil de Usuario</a></div>
+         <div class="side-option"><a>Amigos</a></div>
+         <div class="side-option"><a>Cerrar Sesión</a></div>
+
+     </div>
+     <footer class="responsive">
+         <a id="user-profile"><img src="./img/userLogo.png" alt="userlogo" class="icon-large"></a>
+     </footer>
+      
+      `
+      
+  // BOTON BARRA DE NAVEGACIÓN LATERAL
+  document.getElementById("user-profile").addEventListener("click", ()=> {
+    document.getElementById("user-profile-side-nav").style.display = "block";
+  })
+
+  // PARA CERRAR NAVEGACIÓN LATERAL
+  document.getElementById("content").addEventListener("click", ()=> {
+    document.getElementById("user-profile-side-nav").style.display = "none";
+  })
+    } else {
+      // No user is signed in.
+      loginPage();
+    }
+  });
+
+  // PARA VERIFICAR QUE LA AUTENTICACION CON REDIRECCIONAMIENTO SALIO OK
   firebase.auth().getRedirectResult().then(function(result) {
     if (result.credential) {
-      // This gives you a Google Access Token. You can use it to access the Google API.
+      // This gives you a Facebook Access Token. You can use it to access the Facebook API.
       var token = result.credential.accessToken;
       // ...
     }
     // The signed-in user info.
     var user = result.user;
-    console.log(user)
   }).catch(function(error) {
     // Handle Errors here.
     var errorCode = error.code;
@@ -84,3 +143,4 @@ const config = {
 
 
 })
+
